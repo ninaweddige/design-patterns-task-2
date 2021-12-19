57f18a9c5184874adb16ddbeda4407dbc927d4a8
Nina Weddige <nina.weddige@gmail.com>
2021-12-19 11:24:40 +0100

    added examples of tea decorator

--- src/Main.java
+++ src/Main.java
@@ -2,35 +2,57 @@ public class Main {
 
     public static void main(String[] args){
         //One plain coffee
-        HotDrink coffee0 = new Coffee();
-        System.out.println(coffee0.getHotDrinkDescription() +
-                " : " + coffee0.getPrice());
+        HotDrink coffee = new Coffee();
+        System.out.println(coffee.getHotDrinkDescription() +
+                " : " + coffee.getPrice());
 
         //One plain tea
-        HotDrink tea0 = new Tea();
-        System.out.println(tea0.getHotDrinkDescription() +
-                " : " + tea0.getPrice());
+        HotDrink tea = new Tea();
+        System.out.println(tea.getHotDrinkDescription() +
+                " : " + tea.getPrice());
 
         //Coffee with sugar
-        HotDrink coffeeWithSugar = new SugarDecorator(coffee0);
+        HotDrink coffeeWithSugar = new SugarDecorator(coffee);
         System.out.println(coffeeWithSugar.getHotDrinkDescription() +
                 " : " + coffeeWithSugar.getPrice());
-        System.out.println(coffee0.getHotDrinkDescription() +
-                " : " + coffee0.getPrice());
+        System.out.println(coffee.getHotDrinkDescription() +
+                " : " + coffee.getPrice());
 
         //Coffee with cream
-        HotDrink coffeeWithCream = new CreamDecorator(coffee0);
+        HotDrink coffeeWithCream = new CreamDecorator(coffee);
         System.out.println(coffeeWithCream.getHotDrinkDescription() +
                 " : " + coffeeWithCream.getPrice());
-        System.out.println(coffee0.getHotDrinkDescription() +
-                " : " + coffee0.getPrice());
+        System.out.println(coffee.getHotDrinkDescription() +
+                " : " + coffee.getPrice());
 
         //Coffee with sugar and two creams
         HotDrink coffeeWithOneSugarOneCream = new CreamDecorator(coffeeWithSugar);
         HotDrink coffeeWithOneSugarTwoCreams = new CreamDecorator(coffeeWithOneSugarOneCream);
         System.out.println(coffeeWithOneSugarTwoCreams.getHotDrinkDescription() +
                 " : " + coffeeWithOneSugarTwoCreams.getPrice());
-        System.out.println(coffee0.getHotDrinkDescription() +
-                " : " + coffee0.getPrice());
+        System.out.println(coffee.getHotDrinkDescription() +
+                " : " + coffee.getPrice());
+
+        //Tea with sugar
+        HotDrink teaWithSugar = new SugarDecorator(tea);
+        System.out.println(teaWithSugar.getHotDrinkDescription() +
+                " : " + teaWithSugar.getPrice());
+        System.out.println(tea.getHotDrinkDescription() +
+                " : " + tea.getPrice());
+
+        //Tea with cream
+        HotDrink teaWithCream = new CreamDecorator(tea);
+        System.out.println(teaWithCream.getHotDrinkDescription() +
+                " : " + teaWithCream.getPrice());
+        System.out.println(tea.getHotDrinkDescription() +
+                " : " + tea.getPrice());
+
+        //Tea with sugar and two creams
+        HotDrink teaWithOneSugarOneCream = new SugarDecorator(teaWithCream);
+        HotDrink teaWithOneSugarTwoCreams = new CreamDecorator(teaWithOneSugarOneCream);
+        System.out.println(teaWithOneSugarTwoCreams.getHotDrinkDescription() +
+                " : " + teaWithOneSugarTwoCreams.getPrice());
+        System.out.println(tea.getHotDrinkDescription() +
+                " : " + tea.getPrice());
     }
 }
